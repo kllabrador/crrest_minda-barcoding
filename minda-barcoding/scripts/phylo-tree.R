2c42050c327e93675cff0d2c2e2e3e4e8532f55f
kllabrador <kllabrador@up.edu.ph>
2023-09-02 18:09:00 -0500

    test

---
+++ minda-barcoding/scripts/phylo-tree.R
@@ -0,0 +1,8 @@
+# Phylogenetic analysis based on R. Follows after the analysis pipeline on [Ph-IRES](https://github.com/Ph-IRES/workshop_data-analysis/tree/main/tutorial_phylogeny_analysis).
+
+
+## There are freely-available software that can guide in the upstream processes (e.g., sequence truncation and clean-up, alignment). @Hall2018 is a good reference for sequence alignment, as well as other topics involving phylogenetics.
+
+## Before you start
+# This pipeline assumes that the sequence has already been aligned. As much as possible, gaps/indels are removed from the alignment so as to avoid errors downstream. Furthermore, ambiguous sequences should have been resolved prior to uploading the data in R.
+
